85a7600685dda70a2710210ef14a71eb4f7e4160
Krezh <17014764+krezh@users.noreply.github.com>
2024-02-16 15:14:23 +0100

    Revert "fix(github-release): update derailed/k9s ( 0.31.8 → v0.31.9 ) (#8)" (#9)
    This reverts commit ca0d31e64d6629264a6863b177459e4aa41f3d70.

--- pkgs/k9s/default.nix
+++ pkgs/k9s/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   pname = "k9s";
   # renovate: datasource=github-tags depName=derailed/k9s
-  version = "v0.31.9";
+  version = "0.31.8";
 
   src = fetchFromGitHub {
     owner = "derailed";
