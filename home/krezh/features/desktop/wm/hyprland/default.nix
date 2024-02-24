390733d77e2729fe3f8d05a90f9b7582ef89c2c8
Krezh <krezh@users.noreply.github.com>
2024-02-24 20:39:48 +0100

    add hyprland conf

--- home/krezh/features/desktop/wm/hyprland/default.nix
+++ home/krezh/features/desktop/wm/hyprland/default.nix
@@ -13,30 +13,7 @@
       ${builtins.readFile ./hypr.conf}
     '';
     settings = {
-      "$mod" = "SUPER";
-      exec-once = [      
-      ];
-      input = {
-        kb_layout = "se";
-        follow_mouse = 1;
-        accel_profile = "flat";
-        sensitivity = 0;
-        touchpad = {
-          natural_scroll = "false";
-        };
-      };
-      monitor = [
-        ",preferred,auto,1"
-      ];
-      bind = [
-        "$mod,        RETURN, exec, wezterm"
-        "$mod, 	      L,      exec, hyprlock"
-        "$mod,        Q,      killactive"
-        "$mod,        V,      togglefloating"
-        "$mod SHIFT,  LEFT,    movewindow, l"
-        "$mod SHIFT,  RIGHT,   movewindow, r"
-        "$mod SHIFT,  UP,      movewindow, u"
-        "$mod SHIFT,  RIGHT,   movewindow, d"
+      exec-once = [
       ];
     };
   };
