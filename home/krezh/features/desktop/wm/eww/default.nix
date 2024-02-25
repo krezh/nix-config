bbbd62fd0bd65e4211a413291db3c0e1ac7e7642
Krezh <krezh@users.noreply.github.com>
2024-02-25 13:24:11 +0100

    Add eww

---
+++ home/krezh/features/desktop/wm/eww/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  programs.eww = {
+    enable = true;
+    package = pkgs.eww-wayland;
+    configDir = ./eww;
+  };
+
+  home.packages = [
+    pkgs.xclip
+    pkgs.wmctrl
+    pkgs.light
+    pkgs.brightnessctl
+  ];
+}
