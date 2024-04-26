8a5a502c282c153850dc5b92add7cd66d6ff45eb
Krezh <krezh@users.noreply.github.com>
2024-04-26 14:28:26 +0200

    woops

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -1,5 +1,5 @@
 # This file (and the global directory) holds config that i use on all hosts
-{ inputs, outputs, lib, ... }: {
+{ inputs, outputs, lib, pkgs, ... }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
     ./locale.nix
