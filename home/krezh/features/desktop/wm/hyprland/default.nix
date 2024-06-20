b8d9f75f57468445e3c92dc15375369f3b4a1ca8
Krezh <krezh@users.noreply.github.com>
2024-06-20 19:21:52 +0200

    feat: Enable clipman pick with the V keybinding

--- home/krezh/features/desktop/wm/hyprland/default.nix
+++ home/krezh/features/desktop/wm/hyprland/default.nix
@@ -8,7 +8,6 @@
   imports = [ ];
   wayland.windowManager.hyprland = {
     enable = true;
-    # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     systemd.enable = true;
     xwayland.enable = true;
     extraConfig = ''
