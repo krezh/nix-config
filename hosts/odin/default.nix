882d7495f7f9ce7363ccd24257f2e50e79b4dd2b
Krezh <17014764+krezh@users.noreply.github.com>
2024-06-25 15:32:04 +0200

    comment disko code

--- hosts/odin/default.nix
+++ hosts/odin/default.nix
@@ -27,36 +27,36 @@ in
 
   programs.gdk-pixbuf.modulePackages = [ pkgs.librsvg ];
 
-  disko.devices = {
-    disk = {
-      vdb = {
-        device = "/dev/nvme0n1";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "1G";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
-        };
-      };
-    };
-  };
+  # disko.devices = {
+  #   disk = {
+  #     vdb = {
+  #       device = "/dev/nvme0n1";
+  #       type = "disk";
+  #       content = {
+  #         type = "gpt";
+  #         partitions = {
+  #           ESP = {
+  #             type = "EF00";
+  #             size = "1G";
+  #             content = {
+  #               type = "filesystem";
+  #               format = "vfat";
+  #               mountpoint = "/boot";
+  #             };
+  #           };
+  #           root = {
+  #             size = "100%";
+  #             content = {
+  #               type = "filesystem";
+  #               format = "ext4";
+  #               mountpoint = "/";
+  #             };
+  #           };
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 
   services.greetd = {
     enable = true;
