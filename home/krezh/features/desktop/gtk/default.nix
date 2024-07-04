12980c7571a08279a8408e3cfeaca5629a485761
Krezh <krezh@users.noreply.github.com>
2024-07-04 14:23:30 +0200

    fix

--- home/krezh/features/desktop/gtk/default.nix
+++ home/krezh/features/desktop/gtk/default.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
 
 {
-  home = {
-    pointerCursor = {
-      gtk.enable = false;
-      x11.enable = false;
-      # package = pkgs.catppuccin-cursors;
-      # name = "mochaBlue";
-      # size = 28;
-    };
-  };
+  # home = {
+  #   pointerCursor = {
+  #     gtk.enable = true;
+  #     x11.enable = true;
+  #     # package = pkgs.catppuccin-cursors;
+  #     # name = "mochaBlue";
+  #     # size = 28;
+  #   };
+  # };
 
   gtk = {
     enable = true;
