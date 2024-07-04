5f31a652cedb1642219ea1e5cd7c775b6e6c3cf1
Krezh <17014764+krezh@users.noreply.github.com>
2024-07-04 08:04:39 +0200

    Update default.nix

--- home/krezh/features/desktop/gtk/default.nix
+++ home/krezh/features/desktop/gtk/default.nix
@@ -5,6 +5,7 @@
     pointerCursor = {
       gtk.enable = true;
       x11.enable = true;
+      catppuccin.enable = true;
       # package = pkgs.catppuccin-cursors;
       # name = "mochaBlue";
       # size = 28;
