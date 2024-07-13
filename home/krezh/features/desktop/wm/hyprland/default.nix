e0670c6fad99f16cc6ddc294be8d8b20f3510e9d
Krezh <krezh@users.noreply.github.com>
2024-07-13 15:55:58 +0200

    updates

--- home/krezh/features/desktop/wm/hyprland/default.nix
+++ home/krezh/features/desktop/wm/hyprland/default.nix
@@ -28,15 +28,93 @@ in
       # Generated from Nix
     '';
     settings = {
+      monitor = [ ",preferred,auto,1" ];
       env = [
         "XCURSOR_SIZE,32"
         "XCURSOR_THEME,macOS-BigSur"
         "HYPRCURSOR_THEME,macOS-BigSur"
         "HYPRCURSOR_SIZE,32"
       ];
+
       cursor = {
         enable_hyprcursor = true;
       };
+
+      general = {
+        gaps_in = 2;
+        gaps_out = 0;
+        border_size = 0;
+        # "col.active_border" = "${catppuccin_border}";
+        # "col.inactive_border" = "${tokyonight_border}";
+        layout = "dwindle";
+        apply_sens_to_raw = 1; # whether to apply the sensitivity to raw input (e.g. used by games where you aim using your mouse)
+      };
+
+      decoration = {
+        rounding = 0;
+        shadow_ignore_window = true;
+        drop_shadow = false;
+        shadow_range = 20;
+        shadow_render_power = 3;
+        # "col.shadow" = "rgb(${oxocarbon_background})";
+        # "col.shadow_inactive" = "${background}";
+        blur = {
+          enabled = true;
+          size = 4;
+          passes = 2;
+          new_optimizations = true;
+          ignore_opacity = true;
+          noise = 1.17e-2;
+          contrast = 1.3;
+          brightness = 1;
+          xray = true;
+        };
+      };
+
+      animations = {
+        enabled = true;
+        bezier = [ "1, 0.23, 1, 0.32, 1" ];
+        animation = [
+          "windows, 1, 5, 1"
+          "windowsIn, 1, 5, 1, slide"
+          "windowsOut, 1, 5, 1, slide"
+          "border, 1, 5, default"
+          "borderangle, 1, 5, default"
+          "fade, 1, 5, default"
+          "workspaces, 1, 5, 1, slidefade 30%"
+        ];
+      };
+
+      misc = {
+        vfr = true; # misc:no_vfr -> misc:vfr. bool, heavily recommended to leave at default on. Saves on CPU usage.
+        vrr = false; # misc:vrr -> Adaptive sync of your monitor. 0 (off), 1 (on), 2 (fullscreen only). Default 0 to avoid white flashes on select hardware.
+        disable_hyprland_logo = true;
+        # disable_splash_rendering = true;
+      };
+
+      dwindle = {
+        pseudotile = true; # enable pseudotiling on dwindle
+        force_split = 0;
+        preserve_split = true;
+        default_split_ratio = 1.0;
+        no_gaps_when_only = false;
+        special_scale_factor = 0.8;
+        split_width_multiplier = 1.0;
+        use_active_for_splits = true;
+      };
+
+      master = {
+        mfact = 0.5;
+        orientation = "right";
+        special_scale_factor = 0.8;
+        new_status = "slave";
+        no_gaps_when_only = false;
+      };
+
+      debug = {
+        damage_tracking = 2; # leave it on 2 (full) unless you hate your GPU and want to make it suffer!
+      };
+
     };
     plugins = [
       inputs.hyprfocus.packages.${pkgs.system}.hyprfocus
