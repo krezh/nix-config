453d9f2a338a7cdbad3c15d057d26f4d5a05889e
Krezh <krezh@users.noreply.github.com>
2024-07-13 15:20:54 +0200

    updates

--- home/krezh/features/desktop/wm/hyprland/default.nix
+++ home/krezh/features/desktop/wm/hyprland/default.nix
@@ -48,6 +48,39 @@ in
   programs.hyprlock = {
     enable = true;
     package = inputs.hyprlock.packages.${pkgs.system}.hyprlock;
+    settings = {
+      general = {
+        disable_loading_bar = true;
+        grace = 300;
+        hide_cursor = true;
+        no_fade_in = false;
+      };
+
+      background = [
+        {
+          monitor = "";
+          path = "screenshot";
+          blur_passes = 3;
+          blur_size = 8;
+        }
+      ];
+
+      input-field = [
+        {
+          size = "200, 50";
+          position = "0, -80";
+          monitor = "";
+          dots_center = true;
+          fade_on_empty = false;
+          font_color = "rgb(202, 211, 245)";
+          inner_color = "rgb(91, 96, 120)";
+          outer_color = "rgb(24, 25, 38)";
+          outline_thickness = 5;
+          placeholder_text = ''<span foreground="##cad3f5">Password...</span>'';
+          shadow_passes = 2;
+        }
+      ];
+    };
   };
 
   services.hypridle = {
@@ -57,7 +90,7 @@ in
       general = {
         after_sleep_cmd = "hyprctl dispatch dpms on";
         ignore_dbus_inhibit = false;
-        lock_cmd = "pidof ${hyprlockFlake}/bin/hyprlock || ${hyprlockFlake}/bin/hyprlock";
+        lock_cmd = "${hyprlockFlake}/bin/hyprlock";
       };
       listener = [
         {
