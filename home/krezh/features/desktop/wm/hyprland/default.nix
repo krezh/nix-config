7a3c9e41568deadd118eca69efb5d16ff8335504
Krezh <krezh@users.noreply.github.com>
2024-07-14 18:01:08 +0200

    refactoring

--- home/krezh/features/desktop/wm/hyprland/default.nix
+++ home/krezh/features/desktop/wm/hyprland/default.nix
@@ -50,6 +50,67 @@ in
         "$mainMod,K,exec,${hyprkeysFlake}/bin/hyprkeys -b -r | anyrun --plugins ${anyrunFlake.stdin}/lib/libstdin.so"
       ];
 
+      input = {
+        kb_layout = "se";
+        follow_mouse = 1;
+        touchpad = {
+          natural_scroll = false;
+        };
+        sensitivity = "0.4"; # -1.0 - 1.0, 0 means no modification.
+      };
+
+      plugins = {
+        hyprfocus = {
+          enabled = false;
+          animate_floating = true;
+          animate_workspacechange = true;
+          focus_animation = "shrink";
+          bezier = [
+            "bezIn, 0.5,0.0,1.0,0.5"
+            "bezOut, 0.0,0.5,0.5,1.0"
+            "overshot, 0.05, 0.9, 0.1, 1.05"
+            "smoothOut, 0.36, 0, 0.66, -0.56"
+            "smoothIn, 0.25, 1, 0.5, 1"
+            "realsmooth, 0.28,0.29,.69,1.08"
+          ];
+          flash = {
+            flash_opacity = 0.95;
+            in_bezier = "realsmooth";
+            in_speed = 0.5;
+            out_bezier = "realsmooth";
+            out_speed = 3;
+          };
+          shrink = {
+            shrink_percentage = 0.99;
+            in_bezier = "realsmooth";
+            in_speed = 1;
+            out_bezier = "realsmooth";
+            out_speed = 2;
+          };
+        };
+
+        touch_gestures = {
+          sensitivity = 1.0;
+          workspace_swipe_fingers = 3;
+          workspace_swipe_edge = "d";
+          long_press_delay = 400;
+          experimental = {
+            send_cancel = 0;
+          };
+        };
+
+        hyprexpo = {
+          columns = 3;
+          gap_size = 5;
+          bg_col = "rgb(111111)";
+          workspace_method = "center current";
+          enable_gesture = true;
+          gesture_fingers = 3;
+          gesture_distance = 300;
+          gesture_positive = true;
+        };
+      };
+
       general = {
         gaps_in = 3;
         gaps_out = 3;
@@ -135,6 +196,6 @@ in
 
   home.packages = with pkgs; [
     inputs.xdg-portal-hyprland.packages.${pkgs.system}.default
-    catppuccin-cursors.mochaLight
+    wlogout
   ];
 }
