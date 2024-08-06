c36f5d6e1194205dc3aa638141bb4e07a3b9ebec
Krezh <krezh@users.noreply.github.com>
2024-08-06 19:44:25 +0200

    talosctl: 1.7.6 -> 1.7.6
    Diff: https://github.com/siderolabs/talos/compare/v1.7.6...v1.7.6

--- pkgs/bin/talosctl/default.nix
+++ pkgs/bin/talosctl/default.nix
@@ -6,18 +6,15 @@
 }:
 buildGoModule rec {
   pname = "talosctl";
-  # renovate: datasource=docker depName=ghcr.io/siderolabs/installer
   version = "1.7.6";
 
   src = fetchFromGitHub {
     owner = "siderolabs";
     repo = "talos";
     rev = "v${version}";
-    # nix-shell -p nix-prefetch-github --run "nix-prefetch-github siderolabs talos --rev v1.7.5"
     hash = "sha256-lmDLlxiPyVhlSPplYkIaS5Uw19hir6XD8MAk8q+obhY=";
   };
 
-  # vendorHash = lib.fakeHash;
   vendorHash = "sha256-8UIey+r1tdVRN1RBK5xxcAzaHb0VFdgenUXSFgoWh1g=";
 
   ldflags = [
