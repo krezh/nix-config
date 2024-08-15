2b6ed68a8f9b7f78ff6bcb850a41deec4c6f234c
Krezh <krezh@users.noreply.github.com>
2024-08-15 15:58:02 +0200

    fix

--- pkgs/bin/gowall/default.nix
+++ pkgs/bin/gowall/default.nix
@@ -8,13 +8,13 @@
 buildGoModule rec {
   pname = "gowall";
   # renovate: datasource=github-releases depName=Achno/gowall
-  version = "0.1.6";
+  version = "0.1.5";
 
   src = fetchFromGitHub {
     owner = "Achno";
     repo = "gowall";
     rev = "v${version}";
-    hash = "sha256-BNksshg1yK3mQuBaC4S3HzwfJ8vW0XxfDkG7YJAF00E=";
+    hash = "sha256-4h7vRu1aqCGccKx2UiLSFNloqf22QUml4BHkKzzdwYA=";
   };
 
   vendorHash = "sha256-jNx4ehew+IBx7M6ey/rT0vb53+9OBVYSEDJv8JWfZIw=";
