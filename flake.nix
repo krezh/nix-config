11c6931e51705f0ea07c11732258885f137c44b9
Krezh <krezh@users.noreply.github.com>
2024-09-16 01:14:38 +0200

    enable cachix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   nixConfig = {
     extra-trusted-substituters = [
-      #"https://krezh.cachix.org"
+      "https://krezh.cachix.org"
       "https://cache.garnix.io"
       "https://nix-cache.plexuz.xyz/krezh"
       "https://nix-community.cachix.org"
@@ -14,7 +14,7 @@
       "https://cosmic.cachix.org"
     ];
     extra-trusted-public-keys = [
-      #"krezh.cachix.org-1:0hGx8u/mABpZkzJEBh/UMXyNon5LAXdCRqEeVn5mff8="
+      "krezh.cachix.org-1:0hGx8u/mABpZkzJEBh/UMXyNon5LAXdCRqEeVn5mff8="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "krezh:pTHGRGjsJcN7JiWnsL9/CaSIMVQbeqQc/xzLi0YgsxQ="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
