4bd68b81611328742a5ce3e21e0aaba14fd03574
Krezh <17014764+krezh@users.noreply.github.com>
2024-10-27 16:42:25 +0100

    Update default.nix

--- home/krezh/features/desktop/apps/terminal/wezterm/default.nix
+++ home/krezh/features/desktop/apps/terminal/wezterm/default.nix
@@ -1,21 +1,8 @@
 { inputs, pkgs, ... }:
-###########################################################
-#
-# Wezterm Configuration
-#
-# Useful Hot Keys for Linux(replace `ctrl + shift` with `cmd` on macOS)):
-#   1. Increase Font Size: `ctrl + shift + =` | `ctrl + shift + +`
-#   2. Decrease Font Size: `ctrl + shift + -` | `ctrl + shift + _`
-#   3. And Other common shortcuts such as Copy, Paste, Cursor Move, etc.
-#
-# Default Keybindings: https://wezfurlong.org/wezterm/config/default-keys.html
-#
-###########################################################
 {
-
   programs.wezterm = {
     enable = true;
-    package = inputs.wezterm.packages.${pkgs.system}.default;
+    package = pkgs.wezterm;
     extraConfig = builtins.readFile ./wezterm.lua;
   };
 }
