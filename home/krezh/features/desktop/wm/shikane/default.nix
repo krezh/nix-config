9a23dc9d2d6b5962ffd43cb97dff23ed3f29f9ca
Krezh <krezh@users.noreply.github.com>
2024-11-17 01:10:17 +0100

    feat(shikane): add docking profile

--- home/krezh/features/desktop/wm/shikane/default.nix
+++ home/krezh/features/desktop/wm/shikane/default.nix
@@ -2,6 +2,32 @@
 {
   hmModules.desktop.shikane = {
     enable = true;
-    config = { };
+    config = {
+      profile = [
+        {
+          name = "default";
+          output = [
+            {
+              name = "eDP-1";
+              enable = true;
+            }
+          ];
+        }
+        {
+          name = "docking";
+          exec = [ ''notify-send "shikane" "profile $SHIKANE_PROFILE_NAME active"'' ];
+          output = [
+            {
+              match = "eDP-1";
+              enable = false;
+            }
+            {
+              match = "/DP-[1-9]/";
+              enable = true;
+            }
+          ];
+        }
+      ];
+    };
   };
 }
