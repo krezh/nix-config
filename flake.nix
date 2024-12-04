b159597501e3f34918d530ad6952f06fc03b9603
Krezh <krezh@users.noreply.github.com>
2024-12-04 01:06:08 +0100

    fix(lic): update lix

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
     hardware.url = "github:nixos/nixos-hardware";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
