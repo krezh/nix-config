47b10bc50e733b9e83d6dfe824b861acbb530d1b
Krezh <krezh@users.noreply.github.com>
2024-12-13 02:53:56 +0100

    feat(nixos): enable Steam and configure firewall settings for Remote Play and game transfers

--- hosts/thor/nixos-configuration.nix
+++ hosts/thor/nixos-configuration.nix
@@ -47,6 +47,13 @@
     };
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   nixosModules.desktop.battery.enable = true;
   nixosModules.desktop.openssh.enable = true;
 
