093929e8eedc112b3d60127208587e3cd3aa23ea
Krezh <17014764+krezh@users.noreply.github.com>
2024-12-13 16:14:44 +0100

    Update shell.nix
    Signed-off-by: Krezh <17014764+krezh@users.noreply.github.com>

--- shell.nix
+++ shell.nix
@@ -17,14 +17,14 @@
       }
       {
         name = "partition";
-        command = "sudo nix --experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko --flake github:krezh/nix-config#odin";
-        help = "Will partition the disk (Odin)";
+        command = "sudo nix --experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko --flake github:krezh/nix-config#thor";
+        help = "Will partition the disk (Thor)";
         category = "Nix";
       }
       {
         name = "install";
-        command = "sudo nix --experimental-features 'nix-command flakes' run 'github:nix-community/disko/latest#disko-install' --experimental-features 'nix-command flakes' -- --flake github:krezh/nix-config#thor";
-        help = "Will partition the disk and install (Thor)";
+        command = "sudo nixos-install --flake github:krezh/nix-config#thor";
+        help = "Will install (Thor)";
         category = "Nix";
       }
     ];
