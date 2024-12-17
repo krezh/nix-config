f1c75f74250550df82eb1ae4c15123c01fe8f3bb
Krezh <krezh@users.noreply.github.com>
2024-12-17 02:03:11 +0100

    some fixes

--- pre-commit.nix
+++ pre-commit.nix
@@ -3,9 +3,6 @@
   check.enable = true;
   settings = {
     hooks = {
-      # nixfmt-rfc-style.enable = true;
-      # deadnix.enable = true;
-      # shellcheck.enable = true;
       check-shebang-scripts-are-executable.enable = true;
       treefmt.enable = true;
     };
