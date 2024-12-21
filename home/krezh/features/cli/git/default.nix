ad5d309725cef299c646aa9c25c25efcad81bf2a
Krezh <krezh@users.noreply.github.com>
2024-12-21 02:24:01 +0100

    add difftastic

--- home/krezh/features/cli/git/default.nix
+++ home/krezh/features/cli/git/default.nix
@@ -1,21 +1,27 @@
 { config, ... }:
 {
-  programs.git = {
-    enable = true;
-    userName = "Krezh";
-    userEmail = "krezh@users.noreply.github.com";
-    extraConfig = {
-      commit.gpgsign = true;
-      pull.rebase = true;
-      rebase.autoStash = true;
-      push.autoSetupRemote = true;
-      gpg.format = "ssh";
-      format.signoff = true;
-      status.submoduleSummary = false;
-      tag.forceSignAnnotated = true;
-      user.signingkey = config.sops.secrets."ssh/privkey".path;
-      init.defaultBranch = "main";
-      url."ssh://git@github.com/".pushInsteadOf = "https://github.com/";
+  programs = {
+    git = {
+      enable = true;
+      userName = "Krezh";
+      userEmail = "krezh@users.noreply.github.com";
+      extraConfig = {
+        commit.gpgsign = true;
+        pull.rebase = true;
+        rebase.autoStash = true;
+        push.autoSetupRemote = true;
+        gpg.format = "ssh";
+        format.signoff = true;
+        status.submoduleSummary = false;
+        tag.forceSignAnnotated = true;
+        user.signingkey = config.sops.secrets."ssh/privkey".path;
+        init.defaultBranch = "main";
+        url."ssh://git@github.com/".pushInsteadOf = "https://github.com/";
+      };
+      difftastic.enable = true;
+      difftastic.display = "inline";
     };
+    lazygit.enable = true;
+    gitui.enable = true;
   };
 }
