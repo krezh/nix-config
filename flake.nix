82e3b1f57f080fb0d98235eddb110c76550c7585
Krezh <krezh@users.noreply.github.com>
2025-04-13 20:43:25 +0200

    feat: change to determinatesystems action

--- flake.nix
+++ flake.nix
@@ -21,6 +21,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
+    conflake.url = "github:ratson/conflake";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
