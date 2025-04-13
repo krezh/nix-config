f506816358f4541ddb1bcd379e75596771b0b2b4
Krezh <krezh@users.noreply.github.com>
2025-04-13 20:46:01 +0200

    test arm

--- flake.nix
+++ flake.nix
@@ -170,6 +170,16 @@
             homeUsers = [ "krezh" ];
           }
           {
+            hostname = "rpi-01";
+            system = "aarch64-linux";
+            homeUsers = [ "krezh" ];
+          }
+          {
+            hostname = "rpi-02";
+            system = "aarch64-linux";
+            homeUsers = [ "krezh" ];
+          }
+          {
             hostname = "nixos-livecd";
             system = "x86_64-linux";
             homeUsers = [ ];
