73078c7b1263fe7c991f6e7363479509cc94ebef
Krezh <17014764+krezh@users.noreply.github.com>
2025-11-10 20:32:59 +0100

    Revert "fix(github-release): update mirceanton/talswitcher ( v2.2.3 → v2.2.4 …" (#671)
    This reverts commit 1472b91e98a72b4f19f7798c325b9283ec44ea87.

--- pkgs/bin/talswitcher/default.nix
+++ pkgs/bin/talswitcher/default.nix
@@ -7,16 +7,16 @@
 buildGoModule rec {
   pname = "talswitcher";
   # renovate: datasource=github-releases depName=mirceanton/talswitcher
-  version = "2.2.4";
+  version = "2.2.3";
 
   src = fetchFromGitHub {
     owner = "mirceanton";
     repo = "talswitcher";
     rev = "v${version}";
-    hash = "sha256-Fv/Tw5DM+OtJc/3f3KL3PdhGbiCirl1Xp3t6zYXckYU=";
+    hash = "sha256-Lwkpgz89bmVT4PmnigIfi4xHEaNYH6HZq5gbS/zrPw4=";
   };
 
-  vendorHash = "sha256-uKX/bV46pW1wYDt1Oo5bHPd04f+tYoqGl43/+BA0RSA=";
+  vendorHash = "sha256-HGwIEgVbfZMsT/x1SNvycxBp/MGhV/++NcYugtwgsyk=";
 
   # Make build write to a writable tempdir instead of /homeless-shelter
   preBuild = ''
