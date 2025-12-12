04140d5b1573ad7a90bcb7916e76ff78b73f1016
Krezh <krezh@users.noreply.github.com>
2025-12-12 15:41:43 +0100

    configure garnix

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
   nixConfig = {
     extra-trusted-substituters = [
       "https://nix-cache.plexuz.xyz/krezh"
+      "https://cache.garnix.io"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "krezh:adc/M3YDasyRetmBf00qt/A83GBG+bGzehg+CLKqvCI="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
