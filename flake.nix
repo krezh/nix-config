ad4b1064cea794d6bca96335e5bcd951232e8b92
Krezh <krezh@users.noreply.github.com>
2026-02-15 15:15:52 +0100

    Revert "replace github: with git+https"
    This reverts commit b2a6c31b331a613451d410962f32abd290068f4e.

--- flake.nix
+++ flake.nix
@@ -18,127 +18,127 @@
   };
 
   inputs = {
-    nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-unstable&shallow=1";
-    hardware.url = "git+https://github.com/nixos/nixos-hardware?shallow=1";
-    nix-cachyos-kernel.url = "git+https://github.com/xddxdd/nix-cachyos-kernel?shallow=1";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hardware.url = "github:nixos/nixos-hardware";
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel";
 
     home-manager = {
-      url = "git+https://github.com/nix-community/home-manager?shallow=1";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    flake-parts.url = "git+https://github.com/hercules-ci/flake-parts?shallow=1";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     devshell = {
-      url = "git+https://github.com/numtide/devshell?shallow=1";
+      url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     treefmt-nix = {
-      url = "git+https://github.com/numtide/treefmt-nix?shallow=1";
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     disko = {
-      url = "git+https://github.com/nix-community/disko?shallow=1";
+      url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     catppuccin = {
-      url = "git+https://github.com/catppuccin/nix?shallow=1";
+      url = "github:catppuccin/nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-index = {
-      url = "git+https://github.com/nix-community/nix-index-database?shallow=1";
+      url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     sops-nix = {
-      url = "git+https://github.com/Mic92/sops-nix?shallow=1";
+      url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nvf = {
-      url = "git+https://github.com/notashelf/nvf?shallow=1";
+      url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixos-wsl = {
-      url = "git+https://github.com/nix-community/nixos-wsl?shallow=1";
+      url = "github:nix-community/nixos-wsl";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     zen-browser = {
-      url = "git+https://github.com/0xc000022070/zen-browser-flake?shallow=1";
+      url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     zen-browser-catppuccin = {
-      url = "git+https://github.com/catppuccin/zen-browser?shallow=1";
+      url = "github:catppuccin/zen-browser";
       flake = false;
     };
 
-    nix-gaming.url = "git+https://github.com/fufexan/nix-gaming?shallow=1";
+    nix-gaming.url = "github:fufexan/nix-gaming";
 
     jovian = {
-      url = "git+https://github.com/Jovian-Experiments/Jovian-NixOS?shallow=1";
+      url = "github:Jovian-Experiments/Jovian-NixOS";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     elephant = {
-      url = "git+https://github.com/abenz1267/elephant?shallow=1";
+      url = "github:abenz1267/elephant";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     walker = {
-      url = "git+https://github.com/abenz1267/walker?shallow=1";
+      url = "github:abenz1267/walker";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.elephant.follows = "elephant";
     };
 
     kauth = {
-      url = "git+https://github.com/krezh/kauth?shallow=1";
+      url = "github:krezh/kauth";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     go-overlay = {
-      url = "git+https://github.com/purpleclay/go-overlay?shallow=1";
+      url = "github:purpleclay/go-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     noctalia = {
-      url = "git+https://github.com/noctalia-dev/noctalia-shell?shallow=1";
+      url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-ai-tools = {
-      url = "git+https://github.com/numtide/nix-ai-tools?shallow=1";
+      url = "github:numtide/nix-ai-tools";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.treefmt-nix.follows = "treefmt-nix";
     };
 
     helium = {
-      url = "git+https://github.com/AlvaroParker/helium-nix?shallow=1";
+      url = "github:AlvaroParker/helium-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     niri = {
-      url = "git+https://github.com/sodiboo/niri-flake?shallow=1";
+      url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixos-generators = {
-      url = "git+https://github.com/nix-community/nixos-generators?shallow=1";
+      url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     hyprshutdown = {
-      url = "git+https://github.com/hyprwm/hyprshutdown?shallow=1";
+      url = "github:hyprwm/hyprshutdown";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix4vscode.url = "git+https://github.com/nix-community/nix4vscode?shallow=1";
+    nix4vscode.url = "github:nix-community/nix4vscode";
     nix4vscode.inputs.nixpkgs.follows = "nixpkgs";
   };
 
